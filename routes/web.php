4134fe4454fe238528bbbf909dd10ba3c6b7446c
chenyirou <annie851216@gmail.com>
2017-10-02 17:39:08 +0800

    add new task

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,21 @@ use Illuminate\Http\Request;
      * 增加新的任務
      */
     Route::post('/task', function (Request $request) {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
     });
 
     /**
